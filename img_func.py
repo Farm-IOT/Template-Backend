87550a002c840688c7f38bf13ef367d22b1c2c8e
DSantra92 <santradibbo@gmail.com>
2021-02-09 02:33:45 +0530

    refactor the img_func

--- img_func.py
+++ img_func.py
@@ -1,17 +1,15 @@
+# pre and post processing functions
 import cv2
-from cv2 import rectangle, imread, imwrite
 
 def save_bbox(filename, results):
-    img = imread(filename)
-    label = results[0]['name']
-    labelSize=cv2.getTextSize(label,cv2.FONT_HERSHEY_COMPLEX,0.5,2)
+    img = cv2.imread(filename)
     for result in results:
         if result['type'] == 'bug':
             color = (0, 0, 255)
         else:
             color = (255, 0, 0)
         
-        rectangle(img, result['bbox'][0], result['bbox'][1], color, 2)
+        cv2.rectangle(img, result['bbox'][0], result['bbox'][1], color, 2)
         
         label = result['name']
         labelSize = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2)
@@ -22,4 +20,4 @@ def save_bbox(filename, results):
         cv2.rectangle(img,(_x1,_y1),(_x2,_y2), color, cv2.FILLED)
         cv2.putText(img,label,(_x1 + 2 ,_y2 -2), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255 , 255 , 255),1)
 
-    imwrite(filename, img)
+    cv2.imwrite(filename, img)
